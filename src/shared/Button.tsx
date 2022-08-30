fb761e8bcfacb84bad57a03f1d9d7551f71553f8
Bruno Leonardo Michels <brunolm7@gmail.com>
2022-08-30 10:19:10 -0300

    Change classes

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,13 +1,17 @@
 import Nullstack from 'nullstack'
 
-const darkMode = 'dark:shadow-highlight/20'
-const general =
-  'relative flex-none rounded-md text-sm font-semibold leading-6 py-1.5 px-3 hover:bg-sky-400 bg-sky-500 text-white shadow-sm'
-
 export class Button extends Nullstack {
   render(props) {
     return (
-      <button {...props} class={`${general} ${darkMode} ${props.class ?? ''}`}>
+      <button
+        {...props}
+        class={[
+          'relative flex-none rounded-md text-sm font-semibold leading-6 py-1.5 px-3',
+          'hover:bg-sky-400 bg-sky-500 text-white shadow-sm',
+          'dark:shadow-highlight/20',
+          props.class,
+        ]}
+      >
         {props.children}
       </button>
     )
